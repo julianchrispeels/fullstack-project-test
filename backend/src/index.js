6f98a9c7b4294ca640af3557e393eff5b0ce492f
julianchrispeels <167462333+julianchrispeels@users.noreply.github.com>
2025-03-14 20:19:38 -0300

    Update index.js with app.listen() temporarily

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,8 @@ import sequelize from './database/database.js';
 
 async function main() {
 	await sequelize.sync({ force: false });
-	/*app.listen(PORT, () => {
-		console.log(`Servidor corriendo en http://localhost:${PORT}`);
-	});*/
+	const PORT = process.env.PORT || 3001;
+	app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
 
 main();
