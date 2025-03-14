03ab02eee67db3054af77e1538420dc81df84121
julianchrispeels <julianchrispeels@gmail.com>
2025-03-14 03:35:21 -0300

    port modified

--- backend/src/config/config.env.js
+++ backend/src/config/config.env.js
@@ -7,5 +7,5 @@ export const DB_PORT = process.env.DB_PORT;
 export const DB_NAME = process.env.DB_NAME;
 export const DB_USER = process.env.DB_USER;
 export const DB_PASSWORD = process.env.DB_PASSWORD;
-export const PORT = process.env.PORT;
+export const PORT = process.env.PORT || 5000;
 export const CLIENT_URL = process.env.CLIENT_URL;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 
 function App() {
 
-	const url = 'http://localhost:5000';
+	const url = 'project-server-zeta.vercel.app';
 
 	const [notesArray, setNotesArray] = useState([]);
 	const [selectedNote, setSelectedNote] = useState('');
